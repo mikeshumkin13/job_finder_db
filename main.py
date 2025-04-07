73ece1240dd15fb6062299a224eeae8dfc70bf9a
a12345 <shu.m2010@yandex.ru>
2025-04-07 12:35:28 +0300

    add api_hh

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ from database.create_tables import create_tables
 if __name__ == "__main__":
     create_database()
     create_tables()
+
+from api.hh_api import get_vacancies_by_employer_id  # Импортируем функцию из API
+
+
+if __name__ == "__main__":
+    # Пример ID компании на HH (Яндекс = 1740)
+    employer_id = 1740
+
+    # Получаем вакансии
+    vacancies = get_vacancies_by_employer_id(employer_id, per_page=5)
+
+    # Выводим названия вакансий
+    for i, vacancy in enumerate(vacancies.get("items", []), 1):
+        print(f"{i}. {vacancy['name']}")
